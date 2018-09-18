529829384bc2b3e51cd51e04e43a500ada8a3c05
midas389 <42837405+midas389@users.noreply.github.com>
2018-09-18 12:57:30 +0200

    java2

--- javascript.js
+++ javascript.js
@@ -9,3 +9,47 @@ console.log(b);
 //opdracht 3
 1400=="ik woon in naboo"
 // je bent hier een nummer met een sting aan het vergelijken
+
+//opdrachten conditionals
+//opdracht 1
+var cijfer=6.3;
+if(cijfer <6){
+    console.log("onvoldoende");
+}
+else if(cijfer <7){
+    console.log('voldoende');
+}
+else if(cijfer <9){
+    console.log("goed")
+}
+else if(cijfer >= 9){
+    console.log("uitmuntend")
+}
+//opdracht 2
+
+switch(Math.round(cijfer)){
+    case 4:
+        console.log("onvoldoende");
+        break;
+    case 5:
+        console.log("onvoldoende");
+        break;
+    case 6:
+        console.log("voldoende");
+        break;
+    case 7:
+        console.log("voldoende");
+        break;
+    case 8:
+        console.log("goed");
+        break;
+    case 9:
+        console.log("goed");
+        break;
+    case 10:
+        console.log("uitmuntend");
+        break;
+    
+}
+
+
