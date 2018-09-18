274fc95e3a84d24b6e905fdb2b3b74a92ea0e5c0
midas389 <42837405+midas389@users.noreply.github.com>
2018-09-18 11:44:32 +0200

    java add

---
+++ javascript.js
@@ -0,0 +1,11 @@
+console.log("test test test");
+//oprdacht 1
+var a =3;
+console.log(a % 2);
+//opdracht 2
+var b="Programming is not so cool";
+b = b.replace(" not","");
+console.log(b);
+//opdracht 3
+1400=="ik woon in naboo"
+// je bent hier een nummer met een sting aan het vergelijken
