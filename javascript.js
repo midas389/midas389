97759bfa037853b4c52a64db1287835036445bf9
midas389 <42837405+midas389@users.noreply.github.com>
2018-09-19 14:06:44 +0200

    format document

--- javascript.js
+++ javascript.js
@@ -1,33 +1,35 @@
 console.log("test test test");
 //oprdacht 1
-var a =3;
+var a = 3;
 console.log(a % 2);
 //opdracht 2
-var b="Programming is not so cool";
-b = b.replace(" not","");
+var b = "Programming is not so cool";
+b = b.replace(" not", "");
 console.log(b);
 //opdracht 3
-1400=="ik woon in naboo"
+1400 == "ik woon in naboo"
 // je bent hier een nummer met een sting aan het vergelijken
+
+
 
 //opdrachten conditionals
 //opdracht 1
-var cijfer=6.3;
-if(cijfer <6){
+var cijfer = 6.3;
+if (cijfer < 6) {
     console.log("onvoldoende");
 }
-else if(cijfer <7){
+else if (cijfer < 7) {
     console.log('voldoende');
 }
-else if(cijfer <9){
+else if (cijfer < 9) {
     console.log("goed")
 }
-else if(cijfer >= 9){
+else if (cijfer >= 9) {
     console.log("uitmuntend")
 }
 //opdracht 2
 
-switch(Math.round(cijfer)){
+switch (Math.round(cijfer)) {
     case 4:
         console.log("onvoldoende");
         break;
@@ -51,20 +53,20 @@ switch(Math.round(cijfer)){
         break;
 }
 // opdarcht 3
-purchasedbook= true;
+purchasedbook = true;
 job = "teacher";
 intrain = false;
 
-if(purchasedbook == true && job == "teacher" && intrain== true){
-console.log("finally i can enjoy my book")
+if (purchasedbook == true && job == "teacher" && intrain == true) {
+    console.log("finally i can enjoy my book")
 }
-else if(purchasedbook == true && intrain == true || job=="teacher"){
+else if (purchasedbook == true && intrain == true || job == "teacher") {
     console.log("don't need to be a teacher to read a book")
 }
-else if(purchasedbook== false){
+else if (purchasedbook == false) {
     console.log("can't read a book when you dont have one ")
 }
-else if(intrain==true){
+else if (intrain == true) {
     console.log("time to relax")
 }
 
